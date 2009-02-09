b3bc2afd56f5d9d275a8d77cda0cece5b477195e
nobeans <nobeans@gmail.com>
2009-02-10 02:51:53 +0900

    YUI-Calendarで、指定された日付がある場合はその日付をデフォルトで選択した状態になるようにした。

--- web-app/js/calendar.js
+++ web-app/js/calendar.js
@@ -4,46 +4,72 @@
     YAHOO.my.months = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"];
     YAHOO.my.dayOfWeek = ["日", "月", "火", "水", "木", "金", "土"];
     YAHOO.my.baseId = "period-oneday-date";
+    YAHOO.my.calendarId = YAHOO.my.baseId + "-calendar"
+    YAHOO.my.buttonId = YAHOO.my.baseId + "-button"
+    YAHOO.my.textId = YAHOO.my.baseId + "-text"
 
     YAHOO.util.Event.addListener(window, "load", initDoc);
     function initDoc() {
         // カレンダの初期化
-        YAHOO.my.calendar = new YAHOO.widget.Calendar("calendar", YAHOO.my.baseId + "-calendar", { title:"日付を選択してください", close:true });
-        YAHOO.my.calendar.selectEvent.subscribe(handleSelect, YAHOO.my.calendar, true);
-        YAHOO.my.calendar.cfg.setProperty("MDY_YEAR_POSITION",  1);
-        YAHOO.my.calendar.cfg.setProperty("MDY_MONTH_POSITION", 2);
-        YAHOO.my.calendar.cfg.setProperty("MY_YEAR_POSITION",  1);
-        YAHOO.my.calendar.cfg.setProperty("MY_MONTH_POSITION", 2);
-        YAHOO.my.calendar.cfg.setProperty("MDY_DAY_POSITION",  3);
-        YAHOO.my.calendar.cfg.setProperty("MONTHS_SHORT",   YAHOO.my.months);
-        YAHOO.my.calendar.cfg.setProperty("MONTHS_LONG",    YAHOO.my.months);
-        YAHOO.my.calendar.cfg.setProperty("WEEKDAYS_1CHAR", YAHOO.my.dayOfWeek);
-        YAHOO.my.calendar.cfg.setProperty("WEEKDAYS_SHORT", YAHOO.my.dayOfWeek);
-        YAHOO.my.calendar.cfg.setProperty("WEEKDAYS_MEDIUM",YAHOO.my.dayOfWeek);
-        YAHOO.my.calendar.cfg.setProperty("WEEKDAYS_LONG",  YAHOO.my.dayOfWeek);
-        YAHOO.my.calendar.render();
+        initCalendar();
 
         // カレンダ表示ボタンの初期化
-        YAHOO.util.Event.addListener(YAHOO.my.baseId + "-button", "click", handleFocus);
-    }               
+        YAHOO.util.Event.addListener(YAHOO.my.buttonId, "click", handleFocus);
+    }
+    function initCalendar() {
+        var calendar = new YAHOO.widget.Calendar("calendar", YAHOO.my.calendarId, { title:"日付を選択してください", close:true });
+        calendar.selectEvent.subscribe(handleSelect, YAHOO.my.calendar, true);
+        calendar.cfg.setProperty("MDY_YEAR_POSITION",  1);
+        calendar.cfg.setProperty("MDY_MONTH_POSITION", 2);
+        calendar.cfg.setProperty("MY_YEAR_POSITION",  1);
+        calendar.cfg.setProperty("MY_MONTH_POSITION", 2);
+        calendar.cfg.setProperty("MDY_DAY_POSITION",  3);
+        calendar.cfg.setProperty("MONTHS_SHORT",   YAHOO.my.months);
+        calendar.cfg.setProperty("MONTHS_LONG",    YAHOO.my.months);
+        calendar.cfg.setProperty("WEEKDAYS_1CHAR", YAHOO.my.dayOfWeek);
+        calendar.cfg.setProperty("WEEKDAYS_SHORT", YAHOO.my.dayOfWeek);
+        calendar.cfg.setProperty("WEEKDAYS_MEDIUM",YAHOO.my.dayOfWeek);
+        calendar.cfg.setProperty("WEEKDAYS_LONG",  YAHOO.my.dayOfWeek);
+        calendar.cfg.setProperty("START_WEEKDAY", 1); // 月曜開始
+        YAHOO.my.calendar = calendar;
+    }
 
     function handleFocus() {
+        var calendar = YAHOO.my.calendar;
+
         // カレンダの表示位置をボタンのちょうど下の部分にする。
-        var button = $(YAHOO.my.baseId + "-button");
+        var button = $(YAHOO.my.buttonId);
         var buttonOffset = Position.cumulativeOffset(button);
-        var calendar = $(YAHOO.my.baseId + '-calendar');
-        calendar.style.left = buttonOffset[0] - button.getWidth();
+        var calendarSpan = $(YAHOO.my.calendarId);
+        calendarSpan.style.left = buttonOffset[0] - button.getWidth();
+
+        // 入力済みの日付を取得
+        var textField = document.getElementById(YAHOO.my.textId);
+        var val = textField.value.split('-');
+        var y = parseInt(val[0]), m = parseInt(val[1]), d = parseInt(val[2]);
+        var shown = new Date(y, m - 1, d);
+
+        // 表示設定
+        var pagedate = "", selected = "";
+        if (shown.getFullYear() == y && shown.getMonth() + 1 == m && shown.getDate() == d) {
+            //日付として正しい場合
+            pagedate = y + "/" + m;
+            selected = y + "/" + m + "/" + d;
+        }
+        calendar.cfg.setProperty("pagedate", pagedate); // 表示する年月
+        calendar.cfg.setProperty("selected", selected); // 選択状態の日付
+        calendar.render();
 
         // 表示する。
-        YAHOO.my.calendar.show();
+        calendar.show();
     }               
 
     function handleSelect(type, args, obj) {
         var date = args[0];
         var date = date[0];
         var year = date[0], month = date[1], day = date[2];
-        var textField = document.getElementById(YAHOO.my.baseId + "-text");
-        textField.value = year + "-" + month + "-" + day;
+        var textField = document.getElementById(YAHOO.my.textId);
+        textField.value = year + "-" + (month < 10 ? "0" + month : month) + "-" + (day < 10 ? "0" + day : day);
         YAHOO.my.calendar.hide();
     }
 
