717c6841e344ae13fd1b7a246956bfdf1305e8c7
nobeans <nobeans@gmail.com>
2009-02-16 01:22:59 +0900

    Bootstrapでビルトイン管理者アカウントadminを追加するようにした。
    adminユーザの編集＆削除＆ロール変更を禁止しようかとも思ったが、そこまでのガードはかけないことにした。

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,12 +1,26 @@
+import org.grails.plugins.springsecurity.service.AuthenticateService
+
 class BootStrap {
 
-     def init = { servletContext ->
-        if (Role.count() == 0) {
-            assert new Role(name:'ROLE_USER',  description:'一般ユーザ').save() != null
-            assert new Role(name:'ROLE_ADMIN', description:'管理者').save() != null
-        }
-     }
+	AuthenticateService authenticateService
 
-     def destroy = {
-     }
+    def init = { servletContext ->
+        // ロール定義を追加する。
+        if (Role.count() == 0) {
+            assert new Role(name:'ROLE_USER',  description:'User').save() != null
+            assert new Role(name:'ROLE_ADMIN', description:'Administrator').save() != null
+        }
+
+        // 組み込みの管理者ユーザを追加する。[admin/admin]
+        if (Person.findByLoginName("admin") == null) {
+            def admin = new Person(loginName:"admin", realName:"Administrator", password:authenticateService.passwordEncoder("admin"), enabled:true, nicks:"", color:"")
+            assert admin != null
+            def role = Role.findByName("ROLE_ADMIN")
+            assert role != null
+            role.addToPersons(admin)
+        }
+    }
+
+    def destroy = {
+    }
 } 
