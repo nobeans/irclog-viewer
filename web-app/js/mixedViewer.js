d0874b66ca7bad3c7121cb8adeac422d1f922151
nobeans <nobeans@gmail.com>
2009-02-27 21:46:40 +0900

    MixedViewerでカレンダを選択して非表示にしたあと、再度表示しようとするとカレンダアイコンを2度クリックしなければならない件に対処した。 カレンダアイコンのトグル関数で表示状態を管理しているが、選択時の非表示処理はトグル関数を使用していなかったため。

--- web-app/js/mixedViewer.js
+++ web-app/js/mixedViewer.js
@@ -114,20 +114,19 @@ if (!IRCLOG) IRCLOG = {};
         // 初期化
         Event.observe(window, "load", function() {
             var calendar = IRCLOG.createCalendar(calendarId);
-            Event.observe(buttonId, "click",
-                IRCLOG.createCalendarToggleHandler(calendar, function() { return $(textId).value }, $(textId))
-            );
-            calendar.selectEvent.subscribe(createHandleSelect(calendar), calendar, true);
+            var toggleFunc = IRCLOG.createCalendarToggleHandler(calendar, function() { return $(textId).value }, $(textId));
+            Event.observe(buttonId, "click", toggleFunc);
+            calendar.selectEvent.subscribe(createHandleSelect(calendar, toggleFunc), calendar, true);
         });
 
-        var createHandleSelect = function(calendar) {
+        var createHandleSelect = function(calendar, toggleFunc) {
             return function(type, args, obj) {
                 var date = args[0];
                 var date = date[0];
                 var year = date[0], month = date[1], day = date[2];
                 var textField = $(textId);
                 textField.value = year + "-" + (month < 10 ? "0" + month : month) + "-" + (day < 10 ? "0" + day : day);
-                calendar.hide();
+                toggleFunc(); // このトグル関数で非表示にしないと内部状態がずれてしまうため。
             }
         }
     })();
