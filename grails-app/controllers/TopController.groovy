dab42e0b35914cd7242b64d7f257c6c6fca464b4
nobeans <nobeans@gmail.com>
2009-02-28 19:26:12 +0900

    コメントアウトしていた古い行を削除した。

--- grails-app/controllers/TopController.groovy
+++ grails-app/controllers/TopController.groovy
@@ -2,7 +2,6 @@ class TopController extends Base {
 
     def index = {
         flash.message = flash.message // メッセージがあった場合は引き継ぐ
-        //redirect(url:'/irclog/viewer')
         redirect(uri: config.irclog.viewer.defaultTargetUrl)
     }
 }
