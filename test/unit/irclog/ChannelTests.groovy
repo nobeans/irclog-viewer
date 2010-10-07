de60081551b0627ab3d35ec15e5b7ed83bb7473e
nobeans <nobeans@gmail.com>
2010-10-07 21:19:55 +0900

    added test methods to ChannelTests

--- test/unit/irclog/ChannelTests.groovy
+++ test/unit/irclog/ChannelTests.groovy
@@ -1,17 +1,43 @@
 package irclog
 
+import com.sun.net.httpserver.Filter.Chain;
 import grails.test.*
 
 class ChannelTests extends GrailsUnitTestCase {
+    
     protected void setUp() {
         super.setUp()
+        mockDomain(Channel)
+        setUpDatabase()
     }
-
-    protected void tearDown() {
-        super.tearDown()
+    
+    void testValidate_OK() {
+        Channel channel = createChannel("#test")
+        assert channel.validate()
     }
-
-    void testSomething() {
-
+    
+    void testValidate_NG_notStartingWithHash() {
+        Channel channel = createChannel("test")
+        assert channel.validate() == false
+    }
+    
+    void testFindAll() {
+        def actual = Channel.findAll()
+        assert actual.size() == 2
+        println actual
+    }
+    
+    private Channel createChannel(name) {
+        def mockChannel = new Channel(name:name)
+        mockChannel.description = "説明文です"
+        mockChannel.isPrivate = true
+        mockChannel.isArchived = true
+        mockChannel.secretKey = "1234"
+        mockChannel
+    }
+    
+    private setUpDatabase() {
+        assert createChannel("#prepared1").save()
+        assert createChannel("#prepared2").save()
     }
 }
