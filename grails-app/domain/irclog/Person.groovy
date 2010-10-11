0986afd1765796f8a075f1aaa14bbe3bf17ae6ca
nobeans <nobeans@gmail.com>
2010-10-11 21:45:20 +0900

    tweaked white spacds and line breaks in Person

--- grails-app/domain/irclog/Person.groovy
+++ grails-app/domain/irclog/Person.groovy
@@ -10,40 +10,36 @@ class Person {
     String nicks // commpa separated nicks
     String color // for coloring on screen
 
-    boolean isAccountExpired()  { false }
-    boolean isAccountLocked()   { false }
-    boolean isPasswordExpired() { false }
-
     static hasMany = [channels:Channel, roles:Role]
-    
+
     static belongsTo = Role
-    
+
     static transients = [ 'repassword',
         'admin', 'accountExpired', 'accountLocked', 'passwordExpired'] // required because there is isXxxx method which is recognized as properties wrongly
 
     static constraints = {
         loginName(blank:false, matches:"[a-zA-Z0-9_-]{3,}+", unique:true, maxSize:100)
         realName(blank:false, unique:true, maxSize:100)
-        password(blank:false, minSize:6, maxSize:100, validator:{ val, obj -> 
-            obj.repassword == val
-        })
+        password(blank:false, minSize:6, maxSize:100, validator:{ val, obj -> obj.repassword == val })
         nicks(matches:"[ 0-9a-zA-Z_-]*", validator:{ val, obj ->
-            val == "" || val.split(/ +/).every{ 
-                it.startsWith(obj.loginName)
-            }
+            val == "" || val.split(/ +/).every{ it.startsWith(obj.loginName) }
         }, maxSize:200)
         color(matches:"#[0-9A-Fa-f]{3}|#[0-9A-Fa-f]{6}")
         enabled()
         roles(size:1..1)
         channels()
     }
-    
+
     static mapping = {
         roles(column:'roles_id', joinTable:'role_person')
         channels(column:'person_channels_id', joinTable:'person_channel')
     }
-    
+
     boolean isAdmin() {
         return (roles?.find{it.name == "ROLE_ADMIN"}) != null
     }
+    boolean isAccountExpired()  { false }
+    boolean isAccountLocked()   { false }
+    boolean isPasswordExpired() { false }
+
 }
