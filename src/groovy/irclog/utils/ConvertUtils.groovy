bdb4cd61403d72a6ed29286a29e430d8bd548ce0
nobeans <nobeans@gmail.com>
2010-10-18 01:11:24 +0900

    added ConvertUtils.toDate and test case for all methods in ConvertUtils

--- src/groovy/irclog/utils/ConvertUtils.groovy
+++ src/groovy/irclog/utils/ConvertUtils.groovy
@@ -2,11 +2,22 @@ package irclog.utils
 
 class ConvertUtils {
 
-    static toInteger(value, defaultValue=null) {
+    static toInteger(value, Integer defaultValue=null) {
         (value?.isInteger()) ? value.toInteger() : defaultValue
     }
 
-    static toLong(value, defaultValue=null) {
+    static toLong(value, Long defaultValue=null) {
         (value?.isLong()) ? value.toLong() : defaultValue
+    }
+
+    static toDate(String date) {
+        switch (date) {
+            case ~"[0-9]{4}-[0-9]{2}-[0-9]{2} [0-2]{1}[0-9]{1}:[0-6]{1}[0-9]{1}:[0-6]{1}[0-9]{1}": // not strict
+                return new java.text.SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse(date)
+            case ~"[0-9]{4}-[0-9]{2}-[0-9]{2}": // not strict
+                return new java.text.SimpleDateFormat("yyyy-MM-dd").parse(date)
+            default:
+                throw new IllegalArgumentException("Cannot parse as 'yyyy-MM-dd hh:mm:ss' or 'yyyy-MM-dd'")
+        }
     }
 }
