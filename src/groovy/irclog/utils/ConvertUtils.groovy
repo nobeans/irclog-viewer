85919e4433af7a4d52b4aca00219b94d21e2be87
Y.Nakano <ynak@jggug.org>
2010-10-22 14:28:13 +0900

    tweaked ConvertUtils' implementations by using GDK methods

--- src/groovy/irclog/utils/ConvertUtils.groovy
+++ src/groovy/irclog/utils/ConvertUtils.groovy
@@ -13,9 +13,9 @@ class ConvertUtils {
     static Date toDate(String date) {
         switch (date) {
             case ~"[0-9]{4}-[0-9]{2}-[0-9]{2} [0-2]{1}[0-9]{1}:[0-6]{1}[0-9]{1}:[0-6]{1}[0-9]{1}": // not strict
-                return new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(date)
+                return Date.parse("yyyy-MM-dd HH:mm:ss", date)
             case ~"[0-9]{4}-[0-9]{2}-[0-9]{2}": // not strict
-                return new java.text.SimpleDateFormat("yyyy-MM-dd").parse(date)
+                return Date.parse("yyyy-MM-dd", date)
             default:
                 throw new IllegalArgumentException("Cannot parse as 'yyyy-MM-dd HH:mm:ss' or 'yyyy-MM-dd'")
         }
@@ -28,17 +28,12 @@ class ConvertUtils {
     }
 
     static Date resetTimeToOrigin(Date date) {
-        if (!date) return null
-        def cal = Calendar.getInstance()
-        cal.time = date
-        return resetTimeToOrigin(cal).time
+        date?.clearTime()
+        return date
     }
 
     static Calendar resetTimeToOrigin(Calendar cal) {
-        cal.set(Calendar.HOUR_OF_DAY, 0)
-        cal.set(Calendar.MINUTE, 0)
-        cal.set(Calendar.SECOND, 0)
-        cal.set(Calendar.MILLISECOND, 0)
+        cal?.clearTime()
         return cal
     }
 }
