8b0f0eef518938a3680e75a6f747d1e97e3b032d
Yasuharu NAKANO <ynak@jggug.org>
2010-10-27 02:31:14 +0900

    added test cases to the search method in a point of view of period (others not yet)

--- test/integration/irclog/IrclogSearchServiceTests.groovy
+++ test/integration/irclog/IrclogSearchServiceTests.groovy
@@ -9,89 +9,121 @@ class IrclogSearchServiceTests extends GrailsUnitTestCase {
     def irclogSearchService
     def ch1, ch2, ch3
     def user1, user2, user3, admin
+    def today
 
     void setUp() {
         super.setUp()
+        
         setUpChannel()
         setUpPerson()
         setUpRelationBetweenPersonAndChannel()
-    }
-
-    void tearDown() {
-        super.tearDown()
+        
+        today = DateUtils.toDate("2011-01-01 12:34:56")
+        mockFor(DateUtils).demand.static.getToday(1..100) {-> return today }
     }
 
     void testSearch_period_all() {
         // Setup
-        mockFor(DateUtils).demand.static.getToday {-> return DateUtils.toDate("2011-01-01 00:00:01") }
         def expected = []
-        expected << saveIrclog(ch2, "2010-12-31 00:00:01", "user1", "PRIVMSG")
-        expected << saveIrclog(ch2, "2009-12-31 00:00:01", "user1", "PRIVMSG")
-        expected << saveIrclog(ch2, "2008-12-31 00:00:01", "user1", "PRIVMSG")
+        expected << saveIrclog(time:"2010-12-31 12:34:56")
+        expected << saveIrclog(time:"2009-12-31 12:34:56")
+        expected << saveIrclog(time:"2008-12-31 12:34:56")
         // Exercise
         def criterion = createCriterion(period:'all')
         def actual = irclogSearchService.search(user3, criterion, [sort:"time"], 'desc')
         // Verify
         assert actual.list == expected
-        assert actual.totalCount == 3
+        assert actual.totalCount == expected.size()
     }
 
     void testSearch_period_year() {
         // Setup
-        mockFor(DateUtils).demand.static.getToday(1..5) {-> DateUtils.toDate("2011-01-01 00:00:01") }
         def expected = []
-        expected << saveIrclog(ch2, "2010-12-31 00:00:01", "user1", "PRIVMSG")
-        expected << saveIrclog(ch2, "2010-01-01 00:00:01", "user1", "PRIVMSG")
-        expected << saveIrclog(ch2, "2010-01-01 00:00:00", "user1", "PRIVMSG")
-        saveIrclog(ch2, "2009-12-31 00:00:00", "user1", "PRIVMSG")
+        expected << saveIrclog(time:"2010-12-31 12:34:56")
+        expected << saveIrclog(time:"2010-01-01 12:34:56")
+        expected << saveIrclog(time:"2010-01-01 00:00:00")
+        saveIrclog(time:"2009-12-31 23:59:59")
         // Exercise
         def criterion = createCriterion(period:'year')
         def actual = irclogSearchService.search(user3, criterion, [sort:"time"], 'desc')
         // Verify
         assert actual.list == expected
-        assert actual.totalCount == 3
+        assert actual.totalCount == expected.size()
     }
 
     void testSearch_period_halfyear() {
         // Setup
-        mockFor(DateUtils).demand.static.getToday(1..5) {-> DateUtils.toDate("2011-01-01 00:00:01") }
         def expected = []
-        expected << saveIrclog(ch2, "2010-12-31 00:00:01", "user1", "PRIVMSG")
-        expected << saveIrclog(ch2, "2010-07-01 00:00:00", "user1", "PRIVMSG")
-        saveIrclog(ch2, "2010-06-30 00:00:00", "user1", "PRIVMSG")
+        expected << saveIrclog(time:"2010-12-31 12:34:56")
+        expected << saveIrclog(time:"2010-07-01 00:00:00")
+        saveIrclog(time:"2010-06-30 23:59:59")
         // Exercise
         def criterion = createCriterion(period:'halfyear')
         def actual = irclogSearchService.search(user3, criterion, [sort:"time"], 'desc')
         // Verify
         assert actual.list == expected
-        assert actual.totalCount == 2
+        assert actual.totalCount == expected.size()
     }
 
-    //        // 今日(すべて)
-    //        def criterion = [
-    //            period:  'today',
-    //            channel: 'all',
-    //            type:    'filtered',
-    //            nick:    '',
-    //            message: '',
-    //        ]
-    //        // 今日＆時刻指定有りの場合
-    //        def criterion = [
-    //            period:  'today',
-    //            'period-today-time': '12:34',
-    //            channel: 'all',
-    //            type:    'filtered',
-    //            nick:    '',
-    //            message: '',
-    //        ]
-    //        def criterion = [
-    //            period:  'ondeday',
-    //            'period-oneday-date': '2010-10-05',
-    //            channel: 'all',
-    //            type:    'filtered',
-    //            nick:    '',
-    //            message: '',
-    //        ]
+    void testSearch_period_month() {
+        // Setup
+        def expected = []
+        expected << saveIrclog(time:"2010-12-01 12:34:56")
+        expected << saveIrclog(time:"2010-12-01 00:00:00")
+        saveIrclog(time:"2010-11-30 23:59:59")
+        // Exercise
+        def criterion = createCriterion(period:'month')
+        def actual = irclogSearchService.search(user3, criterion, [sort:"time"], 'desc')
+        // Verify
+        assert actual.list == expected
+        assert actual.totalCount == expected.size()
+    }
+
+    void testSearch_period_week() {
+        // Setup
+        def expected = []
+        expected << saveIrclog(time:"2010-12-31 12:34:56")
+        expected << saveIrclog(time:"2010-12-25 00:00:01")
+        expected << saveIrclog(time:"2010-12-25 00:00:00")
+        saveIrclog(time:"2010-12-24 23:59:59")
+        // Exercise
+        def criterion = createCriterion(period:'week')
+        def actual = irclogSearchService.search(user3, criterion, [sort:"time"], 'desc')
+        // Verify
+        assert actual.list == expected
+        assert actual.totalCount == expected.size()
+    }
+
+    void testSearch_period_oneday() {
+        // Setup
+        def expected = []
+        saveIrclog(time:"2010-12-31 00:00:00")
+        expected << saveIrclog(time:"2010-12-24 23:59:59")
+        expected << saveIrclog(time:"2010-12-24 12:34:56")
+        expected << saveIrclog(time:"2010-12-24 00:00:00")
+        saveIrclog(time:"2010-12-23 23:59:59")
+        // Exercise
+        def criterion = createCriterion(period:'oneday', 'period-oneday-date':'2010-12-24')
+        def actual = irclogSearchService.search(user3, criterion, [sort:"time"], 'desc')
+        // Verify
+        assert actual.list == expected
+        assert actual.totalCount == expected.size()
+    }
+
+    void testSearch_period_today() {
+        // Setup
+        def expected = []
+        expected << saveIrclog(time:"2011-01-01 23:59:59")
+        expected << saveIrclog(time:"2011-01-01 12:34:56")
+        expected << saveIrclog(time:"2011-01-01 00:00:00")
+        saveIrclog(time:"2010-12-31 23:59:59")
+        // Exercise
+        def criterion = createCriterion(period:'today')
+        def actual = irclogSearchService.search(user3, criterion, [sort:"time"], 'desc')
+        // Verify
+        assert actual.list == expected
+        assert actual.totalCount == expected.size()
+    }
 
     private setUpChannel() {
         (1..3).each { num ->
@@ -119,9 +151,23 @@ class IrclogSearchServiceTests extends GrailsUnitTestCase {
         user3.addToChannels(ch3)
     }
 
-    private saveIrclog(Channel ch, String dateStr, String nick, String type) {
-        def permaId = "log:${ch.name}:${dateStr}:${nick}:${type}"
-        return createIrclog(permaId:permaId, channelName:ch.name, channel:ch, time:DateUtils.toDate(dateStr), type:type, nick:nick).saveSurely()
+    private saveIrclog(propMap) {
+        if (propMap.time in String) {
+            propMap.time = DateUtils.toDate(propMap.time)
+        }
+        if (propMap.channel) {
+            propMap.channelName = propMap.channel.name
+        }
+        def defaultMap = [
+            channel: ch2,
+            channelName: ch2.name,
+            time: today,
+            nick: "user1",
+            type: "PRIVMSG",
+        ]
+        def mergedMap = defaultMap + propMap
+        mergedMap.permaId = "log:${mergedMap.ch}:${mergedMap.time}:${mergedMap.nick}:${mergedMap.type}"
+        return createIrclog(mergedMap).saveSurely()
     }
 
     private createCriterion(map) {
