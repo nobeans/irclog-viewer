3fda1fd38857ba5e72f2900651bfa01fa6ea80ab
Yasuharu NAKANO <ynak@jggug.org>
2011-09-19 19:27:43 +0900

    wrote PersonTests and improved Person

--- grails-app/domain/irclog/Person.groovy
+++ grails-app/domain/irclog/Person.groovy
@@ -18,15 +18,15 @@ class Person {
         'admin', 'accountExpired', 'accountLocked', 'passwordExpired'] // required because there is isXxxx method which is recognized as properties wrongly
 
     static constraints = {
-        loginName(blank:false, matches:"[a-zA-Z0-9_-]{3,}+", unique:true, maxSize:100)
+        loginName(blank:false, matches:"[a-zA-Z0-9_-]{3,}", unique:true, maxSize:100)
         realName(blank:false, unique:true, maxSize:100)
         password(blank:false, minSize:6, maxSize:100, validator:{ val, obj -> obj.repassword == val })
-        nicks(matches:"[ 0-9a-zA-Z_-]*", validator:{ val, obj ->
+        nicks(matches:"[ 0-9a-zA-Z_-]+", validator:{ val, obj ->
             val == "" || val.split(/ +/).every{ it.startsWith(obj.loginName) }
         }, maxSize:200)
         color(matches:"#[0-9A-Fa-f]{3}|#[0-9A-Fa-f]{6}")
         enabled()
-        roles(size:1..1)
+        roles(nullable:false, size:1..1) // TODO in case of the hasMany's field, it seems that nullable:true is default...
         channels()
     }
 
@@ -38,8 +38,8 @@ class Person {
     boolean isAdmin() {
         return (roles?.find{it.name == "ROLE_ADMIN"}) != null
     }
-    boolean isAccountExpired()  { false }
-    boolean isAccountLocked()   { false }
-    boolean isPasswordExpired() { false }
+    //boolean isAccountExpired()  { false }
+    //boolean isAccountLocked()   { false }
+    //boolean isPasswordExpired() { false }
 
 }
--- test/unit/irclog/PersonTests.groovy
+++ test/unit/irclog/PersonTests.groovy
@@ -2,11 +2,200 @@ package irclog
 
 import grails.test.mixin.*
 import org.junit.*
+import static irclog.utils.DomainUtils.*
 
 @TestFor(Person)
 class PersonTests {
 
-    void testSomething() {
-
+    @Before
+    void setUp() {
+        mockForConstraintsTests(Person, [
+            createPerson(loginName:'EXISTED_loginName', realName:'EXISTED_realName')
+        ])
     }
+
+    @Test
+    void validate_OK() {
+        def person = createPerson()
+        assert person.validate()
+    }
+
+    @Test
+    void validate_NG_loginName_blank() {
+        def person = createPerson(loginName:'')
+        assert person.validate() == false
+        assert person.errors['loginName'] == 'blank'
+    }
+
+    @Test
+    void validate_NG_loginName_unique() {
+        def person = createPerson(loginName:'EXISTED_loginName')
+        assert person.validate() == false
+        assert person.errors['loginName'] == 'unique'
+    }
+
+    @Test
+    void validate_OK_loginName_matches() {
+        assert createPerson(loginName:'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-').validate()
+    }
+
+    @Test
+    void validate_NG_loginName_matches() {
+        def person = createPerson(loginName:'あabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-')
+        assert person.validate() == false
+        assert person.errors['loginName'] == 'matches'
+    }
+
+    @Test
+    void validate_NG_loginName_matches_less() {
+        def person = createPerson(loginName:'x'*2)
+        assert person.validate() == false
+        assert person.errors['loginName'] == 'matches'
+    }
+
+    @Test
+    void validate_OK_loginName_maxSize() {
+        assert createPerson(loginName:'x'*3, realName:'test').validate()
+        assert createPerson(loginName:'x'*100, realName:'test').validate()
+    }
+
+    @Test
+    void validate_NG_loginName_maxSize_more() {
+        def person = createPerson(loginName:'x'*101)
+        assert person.validate() == false
+        assert person.errors['loginName'] == 'maxSize'
+    }
+
+    @Test
+    void validate_NG_realName_blank() {
+        def person = createPerson(realName:'')
+        assert person.validate() == false
+        assert person.errors['realName'] == 'blank'
+    }
+
+    @Test
+    void validate_NG_realName_unique() {
+        def person = createPerson(realName:'EXISTED_realName')
+        assert person.validate() == false
+        assert person.errors['realName'] == 'unique'
+    }
+
+    @Test
+    void validate_OK_realName_maxSize() {
+        assert createPerson(realName:'x'*3).validate()
+        assert createPerson(realName:'x'*100).validate()
+    }
+
+    @Test
+    void validate_NG_realName_maxSize_more() {
+        def person = createPerson(realName:'x'*101)
+        assert person.validate() == false
+        assert person.errors['realName'] == 'maxSize'
+    }
+
+    @Test
+    void validate_NG_password_blank() {
+        def person = createPerson(password:'')
+        assert person.validate() == false
+        assert person.errors['password'] == 'blank'
+    }
+
+    @Test
+    void validate_OK_password_size() {
+        assert createPerson(password:'x'*6).validate()
+        assert createPerson(password:'x'*100).validate()
+    }
+
+    @Test
+    void validate_NG_password_minSize() {
+        def person = createPerson(password:'x'*5)
+        assert person.validate() == false
+        assert person.errors['password'] == 'minSize'
+    }
+
+    @Test
+    void validate_NG_password_maxSize() {
+        def person = createPerson(password:'x'*101)
+        assert person.validate() == false
+        assert person.errors['password'] == 'maxSize'
+    }
+
+    @Test
+    void validate_OK_password_validator() {
+        assert createPerson(password:'123456', repassword:'123456')
+        assert createPerson(password:'1234567890', repassword:'1234567890')
+    }
+
+    @Test
+    void validate_NG_password_validator() {
+        def person = createPerson(password:'123456', repassword:'12345x')
+        assert person.validate() == false
+        assert person.errors['password'] == 'validator'
+    }
+
+    @Test
+    void validate_OK_nicks_matches_maxSize_startsWith() {
+        assert createPerson(loginName:'LOGIN_NAME', nicks:'LOGIN_NAMEabcdefghijklmnopqrstuvwxyz LOGIN_NAMEABCDEFGHIJKLMNOPQRSTUVWXYZ LOGIN_NAME0123456789_-').validate()
+        assert createPerson(loginName:'LOGIN_NAME',  nicks:'LOGIN_NAME'+'x'*(200 - 'LOGIN_NAME'.size())).validate()
+    }
+
+    @Test
+    void validate_NG_nicks_matches_invalidChar() {
+        def person = createPerson(loginName:'LOGIN_NAME',  nicks:'LOGIN_NAMEあいうえお')
+        assert person.validate() == false
+        assert person.errors['nicks'] == 'matches'
+    }
+
+    @Test
+    void validate_NG_nicks_matches_startsWith() {
+        def person = createPerson(loginName:'LOGIN_NAME',  nicks:'abcd')
+        assert person.validate() == false
+        assert person.errors['nicks'] == 'validator'
+    }
+
+    @Test
+    void validate_NG_nicks_matches_maxSize() {
+        def person = createPerson(loginName:'LOGIN_NAME',  nicks:'LOGIN_NAME'+'x'*(200 - 'LOGIN_NAME'.size() + 1))
+        assert person.validate() == false
+        assert person.errors['nicks'] == 'maxSize'
+    }
+
+    @Test
+    void validate_OK_color_matches() {
+        assert createPerson(color:'#000').validate()
+        assert createPerson(color:'#1ac').validate()
+        assert createPerson(color:'#fff').validate()
+        assert createPerson(color:'#000000').validate()
+        assert createPerson(color:'#123456').validate()
+        assert createPerson(color:'#abcdef').validate()
+    }
+
+    @Test
+    void validate_NG_color_matches() {
+        def person = createPerson(color:'000')
+        assert person.validate() == false
+        assert person.errors['color'] == 'matches'
+    }
+
+    @Test
+    void validate_NG_roles_nullable() {
+        def person = createPerson(roles:null)
+        assert person.validate() == false
+        assert person.errors['roles'] == 'nullable'
+    }
+
+    @Test
+    void validate_NG_roles_size_empty() {
+        def person = createPerson(roles:[])
+        assert person.validate() == false
+        assert person.errors['roles'] == 'size'
+    }
+
+    @Test
+    void validate_NG_roles_size_multiple() {
+        def person = createPerson(roles:[createRole(), createRole()])
+        assert person.validate() == false
+        assert person.errors['roles'] == 'size'
+    }
+
 }
