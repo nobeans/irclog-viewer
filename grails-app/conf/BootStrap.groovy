bdc47af2040e4951a4b8486a9303bd26931a7c57
Yasuharu NAKANO <ynak@jggug.org>
2011-09-20 00:02:08 +0900

    added bootstrap code to insert some records into irclog table

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -2,7 +2,7 @@ import grails.util.Environment
 import irclog.Channel
 import irclog.Role
 import irclog.Person
-import static irclog.utils.DateUtils.*
+import irclog.utils.DateUtils
 import static irclog.utils.DomainUtils.*
 
 class BootStrap {
@@ -42,8 +42,15 @@ class BootStrap {
     private setupForDevelopmentEnv() {
         if (Environment.current == Environment.DEVELOPMENT) { // only in development mode
             saveSurely(createChannel(name:"#test1", isPrivate:true))
-            saveSurely(createChannel(name:"#test2", isPrivate:false, secretKey:""))
+            def channelTest2 = saveSurely(createChannel(name:"#test2", isPrivate:false, secretKey:""))
             saveSurely(createChannel(name:"#test3", isPrivate:true))
+            30.times {
+                (-2..0).each { dateDelta ->
+                    def today = DateUtils.today
+                    saveSurely(createIrclog(channelName:"#test1", channel:channelTest2, type:"PRIVMSG", time:today + dateDelta))
+                    saveSurely(createIrclog(channelName:"#test1", channel:channelTest2, type:"NOTICE", time:today + dateDelta))
+                }
+            }
         }
     }
 }
