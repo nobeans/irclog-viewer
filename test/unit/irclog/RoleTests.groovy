7abc365bcb06266020305c09502c530a7453dd18
Yasuharu NAKANO <ynak@jggug.org>
2011-09-19 17:39:12 +0900

    wrote RoleTests

--- test/unit/irclog/RoleTests.groovy
+++ test/unit/irclog/RoleTests.groovy
@@ -2,11 +2,35 @@ package irclog
 
 import grails.test.mixin.*
 import org.junit.*
+import static irclog.utils.DomainUtils.*
 
 @TestFor(Role)
 class RoleTests {
 
-    void testSomething() {
+    @Before
+    void setUp() {
+        mockForConstraintsTests(Role, [
+            createRole(name:'EXISTED_NAME')
+        ])
+    }
 
+    @Test
+    void validate_Ok() {
+        def role = createRole()
+        assert role.validate()
+    }
+
+    @Test
+    void validate_NG_name_blank() {
+        def role = createRole(name:'')
+        assert role.validate() == false
+        assert role.errors['name'] == 'blank'
+    }
+
+    @Test
+    void validate_NG_name_unique() {
+        def role = createRole(name:'EXISTED_NAME')
+        assert role.validate() == false
+        assert role.errors['name'] == 'unique'
     }
 }
