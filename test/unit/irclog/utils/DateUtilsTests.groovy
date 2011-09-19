f56197f3a08c725d4bd1c6eaae27e790a0e41fe2
Yasuharu NAKANO <ynak@jggug.org>
2011-09-19 23:03:42 +0900

    tweaked DateUtilsTests

--- test/unit/irclog/utils/DateUtilsTests.groovy
+++ test/unit/irclog/utils/DateUtilsTests.groovy
@@ -1,19 +1,25 @@
 package irclog.utils
 
+import grails.test.mixin.*
+import grails.test.mixin.support.*
 import org.junit.*
 
-class DateUtilsTests extends GroovyTestCase {
+@TestMixin(GrailsUnitTestMixin)
+class DateUtilsTests {
 
-    void testToDate_date() {
-        assert DateUtils.toDate("2010-10-18").toString() == "Mon Oct 18 00:00:00 JST 2010"
+    @Test
+    void toDate_dateAndTime() {
+        assertDate "2010-10-18 01:23:45", DateUtils.toDate("2010-10-18 01:23:45")
+        assertDate "2010-10-18 23:59:59", DateUtils.toDate("2010-10-18 23:59:59")
     }
 
-    void testToDate_dateAndTime() {
-        assert DateUtils.toDate("2010-10-18 01:23:45").toString() == "Mon Oct 18 01:23:45 JST 2010"
-        assert DateUtils.toDate("2010-10-18 23:59:59").toString() == "Mon Oct 18 23:59:59 JST 2010"
+    @Test
+    void toDate_date() {
+        assertDate "2010-10-18 00:00:00", DateUtils.toDate("2010-10-18")
     }
 
-    void testToDate_invalid() {
+    @Test
+    void toDate_invalid() {
         [
             "",
             "xx",
@@ -28,28 +34,46 @@ class DateUtilsTests extends GroovyTestCase {
         }
     }
 
-    void testToCalendar_date() {
-        assert DateUtils.toCalendar("2010-10-18").time.toString() == "Mon Oct 18 00:00:00 JST 2010"
+    @Test
+    void toCalendar_String_date() {
+        String date = "2010-10-18"
+        assertDate "2010-10-18 00:00:00", DateUtils.toCalendar(date)
+        assertDate "1234-10-31 00:59:00", DateUtils.toCalendar(date, [year:1234, date:31, minute:59])
+        assertDate "1234-12-31 23:59:45", DateUtils.toCalendar(date, [year:1234, month:11, date:31, hourOfDay:23, minute:59, second:45])
     }
 
-    void testToCalendar_dateAndTime() {
-        assert DateUtils.toCalendar("2010-10-18 01:23:45").time.toString() == "Mon Oct 18 01:23:45 JST 2010"
-        assert DateUtils.toCalendar("2010-10-18 23:59:59").time.toString() == "Mon Oct 18 23:59:59 JST 2010"
+    @Test
+    void toCalendar_String_dateAndTime() {
+        String date = "2010-10-18 01:23:45"
+        assertDate "2010-10-18 01:23:45", DateUtils.toCalendar(date)
+        assertDate "1234-10-31 01:59:45", DateUtils.toCalendar(date, [year:1234, date:31, minute:59])
+        assertDate "1234-12-31 23:59:45", DateUtils.toCalendar(date, [year:1234, month:11, date:31, hourOfDay:23, minute:59, second:45])
     }
 
-    void testResetTimeToOrigin_Date() {
+    @Test
+    void toCalendar_Date_dateAndTime() {
+        Date date = Date.parse("yyyy-MM-dd HH:mm:ss", "2010-10-18 01:23:45")
+        assertDate "2010-10-18 01:23:45", DateUtils.toCalendar(date)
+        assertDate "1234-10-31 01:59:45", DateUtils.toCalendar(date, [year:1234, date:31, minute:59])
+        assertDate "1234-12-31 23:59:45", DateUtils.toCalendar(date, [year:1234, month:11, date:31, hourOfDay:23, minute:59, second:45])
+    }
+
+    @Test
+    void resetTimeToOrigin_Date() {
         Date date = DateUtils.toDate("2010-10-18 01:23:45")
         Date actual = DateUtils.resetTimeToOrigin(date)
-        assert actual.toString() == "Mon Oct 18 00:00:00 JST 2010"
+        assertDate "2010-10-18 00:00:00", actual
     }
 
-    void testResetTimeToOrigin_Calendar() {
+    @Test
+    void resetTimeToOrigin_Calendar() {
         Calendar cal = DateUtils.toCalendar("2010-10-18 01:23:45")
         Calendar actual = DateUtils.resetTimeToOrigin(cal)
-        assert actual.time.toString() == "Mon Oct 18 00:00:00 JST 2010"
+        assertDate "2010-10-18 00:00:00", actual
     }
 
-   void testToday_realtime() {
+    @Test
+    void getToday_realtime() {
         def before = new Date()
         def today = DateUtils.getToday()
         def after = new Date()
@@ -57,8 +81,12 @@ class DateUtilsTests extends GroovyTestCase {
         assert today <= after
     }
 
-    void testEpoch() {
+    @Test
+    void getEpoch() {
         assert DateUtils.getEpoch() == new Date(0)
     }
 
+    private static assertDate(String expected, date) {
+        assert date.format('yyyy-MM-dd HH:mm:ss') == expected
+    }
 }
