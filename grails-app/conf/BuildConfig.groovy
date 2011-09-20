814f2c96485103b4e6f26f6360b42f3db50be869
Yasuharu NAKANO <ynak@jggug.org>
2011-09-20 21:41:05 +0900

    upgrade BuildConfig.groovy and moved plugins configurations to it from application.properties

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,23 +1,28 @@
+grails.servlet.version = "2.5" // Change depending on target container compliance (2.5 or 3.0)
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
+
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
         // uncomment to disable ehcache
         // excludes 'ehcache'
     }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    checksums true // Whether to verify checksums on resolve
+
     repositories {
+        inherits true // Whether to inherit repository definitions from plugins
         grailsPlugins()
         grailsHome()
         grailsCentral()
-
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        mavenLocal()
         mavenCentral()
+
+        // uncomment these to enable remote dependency resolution from public Maven repositories
+        //mavenCentral()
+        mavenLocal()
         //mavenRepo "http://snapshots.repository.codehaus.org"
         //mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
@@ -29,6 +34,16 @@ grails.project.dependency.resolution = {
         // runtime 'mysql:mysql-connector-java:5.1.5'
         runtime "postgresql:postgresql:9.0-801.jdbc4"
     }
+
+    plugins {
+        compile ":hibernate:$grailsVersion"
+        compile ":jquery:1.6.1.1"
+        compile ":resources:1.0.2"
+        compile ":spring-security-core:1.2.1"
+
+        build ":tomcat:$grailsVersion"
+    }
 }
 grails.plugin.location."http-nocache" = "./plugins/http-nocache-0.1"
 grails.plugin.location."request-tracelog" = "./plugins/request-tracelog-0.2"
+
