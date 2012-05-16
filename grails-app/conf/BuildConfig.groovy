11bb16db132b5f3032041fb97fdfc20d60c934b9
Yasuharu NAKANO <ynak@jggug.org>
2012-05-17 00:36:43 +0900

    tweaked BuildConfig.groovy

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -21,19 +21,11 @@ grails.project.dependency.resolution = {
         grailsHome()
         grailsCentral()
         mavenCentral()
-
-        // uncomment these to enable remote dependency resolution from public Maven repositories
-        //mavenCentral()
         mavenLocal()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
-        //runtime 'mysql:mysql-connector-java:5.1.5'
         runtime "postgresql:postgresql:9.0-801.jdbc4"
     }
 
