de2b4906a7d4344b9faa8b1b5a06beef5db41c6b
Yasuharu NAKANO <ynak@jggug.org>
2012-07-13 23:35:21 +0900

    added MyPostgreSQLDialect to made hibernate use individual sequences for each domain

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,8 +1,8 @@
 dataSource {
-    pooled = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
+    driverClassName = "org.postgresql.Driver"
+    username = "postgres"
     password = ""
+    dialect = irclog.helper.MyPostgreSQLDialect
 }
 hibernate {
     cache.use_second_level_cache = false
@@ -12,24 +12,23 @@ hibernate {
 // environment specific settings
 environments {
     development {
+        //dataSource {
+        //    dbCreate = "create"
+        //    url = "jdbc:h2:file:db/irclog_dev;MVCC=TRUE"
+        //    //loggingSql = true
+        //}
         dataSource {
             dbCreate = "create"
-            url = "jdbc:h2:file:db/irclog_dev;MVCC=TRUE"
-            //loggingSql = true
-        }
-    }
-    postgres {
-        dataSource {
-            driverClassName = "org.postgresql.Driver"
-            username = "postgres"
-            password = ""
             url = "jdbc:postgresql://localhost:5432/irclog_dev"
-            dbCreate = "create"
             //loggingSql = true
         }
     }
     test {
         dataSource {
+            pooled = true
+            driverClassName = "org.h2.Driver"
+            username = "sa"
+            password = ""
             dbCreate = "create-drop"
             url = "jdbc:h2:mem:irclog_test;MVCC=TRUE"
         }
@@ -37,18 +36,19 @@ environments {
     production {
         dataSource {
             //dbCreate = "update"
-            url = "jdbc:h2:irclog;MVCC=TRUE"
+            url = "jdbc:postgresql://localhost:5432/irclog"
             pooled = true
             properties {
-               maxActive = -1
-               minEvictableIdleTimeMillis=1800000
-               timeBetweenEvictionRunsMillis=1800000
-               numTestsPerEvictionRun=3
-               testOnBorrow=true
-               testWhileIdle=true
-               testOnReturn=true
-               validationQuery="SELECT 1"
+                maxActive = -1
+                minEvictableIdleTimeMillis = 1800000
+                timeBetweenEvictionRunsMillis = 1800000
+                numTestsPerEvictionRun = 3
+                testOnBorrow = true
+                testWhileIdle = true
+                testOnReturn = true
+                validationQuery = "SELECT 1"
             }
+            dialect = irclog.helper.MyPostgreSQLDialect
         }
     }
 }
---
+++ src/groovy/irclog/helper/MyPostgreSQLDialect.groovy
@@ -0,0 +1,31 @@
+package irclog.helper
+
+import org.hibernate.type.Type
+import org.hibernate.dialect.Dialect
+import org.hibernate.dialect.PostgreSQLDialect
+import org.hibernate.id.PersistentIdentifierGenerator
+import org.hibernate.id.SequenceGenerator
+
+class MyPostgreSQLDialect extends PostgreSQLDialect {
+
+    @Override
+    Class<?> getNativeIdentifierGeneratorClass() {
+        TableNameSequenceGenerator.class
+    }
+
+    static class TableNameSequenceGenerator extends SequenceGenerator {
+
+        private static final SEQUENCE_SUFFIX = "seq"
+
+        @Override
+        void configure(Type type, Properties params, Dialect dialect) {
+            if (!params.getProperty(SEQUENCE)) {
+                String tableName = params.getProperty(PersistentIdentifierGenerator.TABLE)
+                if (tableName) {
+                    params.setProperty(SEQUENCE, "${tableName}_id_${SEQUENCE_SUFFIX}")
+                }
+            }
+            super.configure(type, params, dialect)
+        }
+    }
+}
