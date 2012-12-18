de0322f8641bf4ca6b946fcb66a67d17a8a57630
Yasuharu NAKANO <ynak@jggug.org>
2012-12-18 13:03:37 +0900

    upgraded database-migration to 1.2.2

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -47,7 +47,7 @@ grails.project.dependency.resolution = {
 
         build ":tomcat:$grailsVersion"
 
-        runtime ":database-migration:1.2.1"
+        runtime ":database-migration:1.2.2"
 
         compile ':cache:1.0.1'
 
