b1fec9c748b11de51f7e40fb83a6615095087fa8
Yasuharu NAKANO <ynak@jggug.org>
2013-01-08 17:16:10 +0900

    fixed a bug that time of 'time' column was reset to 00:00:00 when paging

--- grails-app/services/irclog/ChannelService.groovy
+++ grails-app/services/irclog/ChannelService.groovy
@@ -1,6 +1,6 @@
 package irclog
 
-import static irclog.utils.DateUtils.*
+import static irclog.utils.DateUtils.toDate
 
 class ChannelService {
 
@@ -136,7 +136,7 @@ class ChannelService {
             }
             order("time", "desc")
             maxResults(1)
-        }.collect { it.time.clearTime() }
+        }.collect { copyDateWithoutTime(it.time) }
         return list.getAt(0)
     }
     /** 現在の日付よりも後で、ログが存在する日付を取得する。*/
@@ -152,7 +152,7 @@ class ChannelService {
             }
             order("time", "asc")
             maxResults(1)
-        }.collect { it.time.clearTime() }
+        }.collect { copyDateWithoutTime(it.time) }
         return list.getAt(0)
     }
     /** 現在の日付よりも後で、ログが存在する最新日付を取得する。*/
@@ -168,7 +168,11 @@ class ChannelService {
             }
             order("time", "desc")
             maxResults(1)
-        }.collect { it.time.clearTime() }
+        }.collect { copyDateWithoutTime(it.time) }
         return list.getAt(0)
+    }
+
+    private static Date copyDateWithoutTime(date) {
+        new Date(date.time).clearTime()
     }
 }
