629961aa9fbf3087d407790531b37840783dbd39
Yasuharu NAKANO <ynak@jggug.org>
2013-02-13 02:04:35 +0900

    updated LoginController/LogoutController to support AJAX

--- grails-app/controllers/irclog/LoginController.groovy
+++ grails-app/controllers/irclog/LoginController.groovy
@@ -1,61 +1,125 @@
 package irclog
 
+import grails.converters.JSON
+import org.codehaus.groovy.grails.plugins.springsecurity.SpringSecurityUtils
+import org.springframework.security.authentication.AccountExpiredException
+import org.springframework.security.authentication.CredentialsExpiredException
 import org.springframework.security.authentication.DisabledException
-import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter
+import org.springframework.security.authentication.LockedException
+import org.springframework.security.core.context.SecurityContextHolder as SCH
+import org.springframework.security.web.WebAttributes
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
+import javax.servlet.http.HttpServletResponse
 
 class LoginController {
 
-    def index() {
-        redirect(action:'auth', params:params)
+    /**
+     * Dependency injection for the authenticationTrustResolver.
+     */
+    def authenticationTrustResolver
+
+    /**
+     * Dependency injection for the springSecurityService.
+     */
+    def springSecurityService
+
+    /**
+     * Default action; redirects to 'defaultTargetUrl' if logged in, /login/auth otherwise.
+     */
+    def index = {
+        if (springSecurityService.isLoggedIn()) {
+            redirect uri: SpringSecurityUtils.securityConfig.successHandler.defaultTargetUrl
+        } else {
+            redirect action: 'auth', params: params
+        }
     }
 
     /**
      * Show the login page.
      */
-    def auth() {
-        if (loggedIn) {
-            flash.message = null
-            flash.errors = null
-            log.info "Logged in by ${principal.username}"
-            redirect(uri: grailsApplication.config.irclog.viewer.defaultTargetUrl)
+    def auth = {
+        def config = SpringSecurityUtils.securityConfig
+        if (springSecurityService.isLoggedIn()) {
+            redirect uri: config.successHandler.defaultTargetUrl
+            return
         }
-        else {
-            render(view:'auth', params:params)
+
+        String view = 'auth'
+        String postUrl = "${request.contextPath}${config.apf.filterProcessesUrl}"
+        render view: view, model: [postUrl: postUrl,
+            rememberMeParameter: config.rememberMe.parameter]
+    }
+
+    /**
+     * The redirect action for Ajax requests.
+     */
+    def authAjax = {
+        response.setHeader 'Location', SpringSecurityUtils.securityConfig.auth.ajaxLoginFormUrl
+        response.sendError HttpServletResponse.SC_UNAUTHORIZED
+    }
+
+    /**
+     * Show denied page.
+     */
+    def denied = {
+        if (springSecurityService.isLoggedIn() &&
+            authenticationTrustResolver.isRememberMe(SCH.context?.authentication)) {
+            // have cookie but the page is guarded with IS_AUTHENTICATED_FULLY
+            redirect action: 'full', params: params
         }
     }
 
-    /** Accedd denied. */
-    def denied() {
-        log.warn "Denied to access: ${request['javax.servlet.forward.request_uri']} by ${principal.username}"
-        def statusCode = request['javax.servlet.error.status_code']
-        if (statusCode) {
-            flash.errors = ['login.accessDenied.error.' + statusCode]
-        } else {
-            flash.errors = ['login.accessDenied.error']
-        }
-        redirect(uri: grailsApplication.config.irclog.viewer.defaultTargetUrl)
+    /**
+     * Login page for users with a remember-me cookie but accessing a IS_AUTHENTICATED_FULLY page.
+     */
+    def full = {
+        def config = SpringSecurityUtils.securityConfig
+        render view: 'auth', params: params,
+            model: [hasCookie: authenticationTrustResolver.isRememberMe(SCH.context?.authentication),
+                postUrl: "${request.contextPath}${config.apf.filterProcessesUrl}"]
     }
 
     /**
      * Callback after a failed login. Redirects to the auth page with a warning message.
      */
-    def authfail() {
-        def loginName = session[UsernamePasswordAuthenticationFilter.SPRING_SECURITY_LAST_USERNAME_KEY]
-        def exception = session[AbstractAuthenticationProcessingFilter.SPRING_SECURITY_LAST_EXCEPTION_KEY]
+    def authfail = {
+        def username = session[UsernamePasswordAuthenticationFilter.SPRING_SECURITY_LAST_USERNAME_KEY]
+        String msg = ''
+        def exception = session[WebAttributes.AUTHENTICATION_EXCEPTION]
         if (exception) {
-            if (exception instanceof DisabledException) {
-                flash.message = "login.loginName.disabled.error"
-            }
-            else {
-                if (loginName == "") {
-                    flash.message = "login.loginName.empty.error"
-                } else {
-                    flash.message = "login.loginName.invalid.error"
-                }
+            if (exception instanceof AccountExpiredException) {
+                msg = g.message(code: "springSecurity.errors.login.expired")
+            } else if (exception instanceof CredentialsExpiredException) {
+                msg = g.message(code: "springSecurity.errors.login.passwordExpired")
+            } else if (exception instanceof DisabledException) {
+                msg = g.message(code: "springSecurity.errors.login.disabled")
+            } else if (exception instanceof LockedException) {
+                msg = g.message(code: "springSecurity.errors.login.locked")
+            } else {
+                msg = g.message(code: "springSecurity.errors.login.fail")
             }
         }
-        flash.loginName = loginName
-        render(view: 'auth')
+
+        if (springSecurityService.isAjax(request)) {
+            render([error: msg] as JSON)
+        } else {
+            flash.message = msg
+            redirect action: 'auth', params: params
+        }
+    }
+
+    /**
+     * The Ajax success redirect url.
+     */
+    def ajaxSuccess = {
+        render([success: true, username: springSecurityService.authentication.name] as JSON)
+    }
+
+    /**
+     * The Ajax denied redirect url.
+     */
+    def ajaxDenied = {
+        render([error: 'access denied'] as JSON)
     }
 }
--- grails-app/controllers/irclog/LogoutController.groovy
+++ grails-app/controllers/irclog/LogoutController.groovy
@@ -3,7 +3,12 @@ package irclog
 import org.codehaus.groovy.grails.plugins.springsecurity.SpringSecurityUtils
 
 class LogoutController {
-    def index() {
-        redirect(uri: SpringSecurityUtils.securityConfig.logout.filterProcessesUrl)
+
+    /**
+     * Index action. Redirects to the Spring security logout uri.
+     */
+    def index = {
+        // TODO put any pre-logout code here
+        redirect uri: SpringSecurityUtils.securityConfig.logout.filterProcessesUrl // '/j_spring_security_logout'
     }
 }
