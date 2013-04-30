1e785acd7f9446a360fa227b175b44ea82490701
Yasuharu NAKANO <ynak@jggug.org>
2013-04-30 15:29:50 +0900

    changed scope of gircbot from 'build' to 'runtime'

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -35,7 +35,7 @@ grails.project.dependency.resolution = {
 
         runtime "postgresql:postgresql:9.0-801.jdbc4"
 
-        build "org.jggug.kobo:gircbot:0.1-SNAPSHOT"
+        runtime "org.jggug.kobo:gircbot:0.1-SNAPSHOT"
 
         compile 'log4j:apache-log4j-extras:1.1', { // for logging
             excludes 'log4j'
