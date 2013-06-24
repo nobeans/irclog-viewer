bc17d21cead407ec2ade4ad695a59be9c23f4f61
Yasuharu NAKANO <ynak@jggug.org>
2013-06-24 14:59:09 +0900

    inited BuildConfig.groovy for 2.3

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -8,6 +8,11 @@ grails.project.target.level = 1.6
 grails.project.source.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
+// uncomment (and adjust settings) to fork the JVM to isolate classpaths
+//grails.project.fork = [
+//   run: [maxMemory:1024, minMemory:64, debug:false, maxPerm:256]
+//]
+
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
@@ -16,6 +21,7 @@ grails.project.dependency.resolution = {
     }
     log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     checksums true // Whether to verify checksums on resolve
+    legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
 
     repositories {
         inherits true // Whether to inherit repository definitions from plugins
@@ -30,8 +36,9 @@ grails.project.dependency.resolution = {
         mavenRepo "https://repository-kobo.forge.cloudbees.com/snapshot/"
         mavenRepo "https://repository-kobo.forge.cloudbees.com/release/"
     }
+
     dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
 
         runtime "postgresql:postgresql:9.0-801.jdbc4"
 
@@ -46,8 +53,8 @@ grails.project.dependency.resolution = {
 
     plugins {
         runtime ":hibernate:$grailsVersion"
-        runtime ":jquery:1.8.0"
-        runtime ":resources:1.1.6"
+        runtime ":jquery:1.8.3"
+        runtime ":resources:1.2"
         runtime ":jquery-ui:1.8.15"
         compile ":spring-security-core:1.2.7.3"
         compile ":quartz:1.0-RC2"
@@ -59,14 +66,14 @@ grails.project.dependency.resolution = {
         if (Environment.current == Environment.PRODUCTION) {
             runtime ":zipped-resources:1.0"
             runtime ":cached-resources:1.0"
-            runtime ":yui-minify-resources:0.1.4"
+            runtime ":yui-minify-resources:0.1.5"
         }
         compile ":lesscss-resources:1.3.1"
         compile ":coffeescript-resources:0.3.7"
 
         build ":tomcat:$grailsVersion"
 
-        runtime ":database-migration:1.2.2"
+        runtime ":database-migration:1.3.2"
 
         compile ':cache:1.0.1'
 
