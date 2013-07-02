10a6bb6c63ea8a69fd7a575675dd17afedfbe919
Yasuharu NAKANO <ynak@jggug.org>
2013-07-03 00:20:14 +0900

    added tagDatabase "v0.4" to v0_4.groovy

--- grails-app/migrations/v0_4.groovy
+++ grails-app/migrations/v0_4.groovy
@@ -22,4 +22,8 @@ databaseChangeLog = {
         addForeignKeyConstraint(baseColumnNames: "ircbot_state_id", baseTableName: "ircbot_state_channel", constraintName: "FKC88FD5C33D3C5A5B", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "ircbot_state", referencesUniqueColumn: "false")
         createSequence(sequenceName: "ircbot_state_id_seq")
     }
+
+    changeSet(author: "irclog", id: "1367341698151-99") {
+        tagDatabase(tag: "v0.4")
+    }
 }
