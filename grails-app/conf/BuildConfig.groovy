65fa253da53b71437d545b8e61daad42f2411f5c
Yasuharu NAKANO <ynak@jggug.org>
2013-09-11 23:33:53 +0900

    upgraded grails to  2.3.0

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,18 +1,29 @@
 import grails.util.Environment
 
-grails.servlet.version = "2.5" // Change depending on target container compliance (2.5 or 3.0)
+grails.servlet.version = "3.0" // Change depending on target container compliance (2.5 or 3.0)
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
+grails.project.work.dir = "target/work"
 grails.project.target.level = 1.6
 grails.project.source.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
-// uncomment (and adjust settings) to fork the JVM to isolate classpaths
-//grails.project.fork = [
-//   run: [maxMemory:1024, minMemory:64, debug:false, maxPerm:256]
-//]
+grails.project.fork = [
+    // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
+    //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
 
+    // configure settings for the test-app JVM, uses the daemon by default
+    test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+    // configure settings for the run-app JVM
+    run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    // configure settings for the run-war JVM
+    war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    // configure settings for the Console UI JVM
+    console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
+]
+
+grails.project.dependency.resolver = "maven" // or ivy
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
@@ -54,30 +65,24 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        runtime ":hibernate:$grailsVersion"
+        runtime ":hibernate:3.6.10.1" // or ":hibernate4:4.1.11.1"
         runtime ":jquery:1.10.2.2"
-        runtime ":resources:1.2"
         runtime ":jquery-ui:1.10.3"
-        compile ":spring-security-core:1.2.7.3"
         compile ":quartz:1.0"
+        compile ":spring-security-core:1.2.7.3"
+        compile ":scaffolding:2.0.0"
+        compile ':cache:1.1.1'
+        runtime ":database-migration:1.3.5"
 
         test(":spock:0.7") {
             exclude "spock-grails-support"
         }
 
-        //if (Environment.current == Environment.PRODUCTION) {
-        //    runtime ":zipped-resources:1.0"
-        //    runtime ":cached-resources:1.0"
-        //    runtime ":yui-minify-resources:0.1.5"
-        //}
+        runtime ":resources:1.2"
         compile ":lesscss-resources:1.3.3"
         compile ":coffeescript-resources:0.3.8"
 
-        build ":tomcat:$grailsVersion"
-
-        runtime ":database-migration:1.3.2"
-
-        compile ':cache:1.0.1'
+        build ":tomcat:7.0.42"
 
         if (Environment.current == Environment.DEVELOPMENT) {
             build ":console-enhancements:1.0"
