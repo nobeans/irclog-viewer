3e3108f81e817c9005f1b42154cd12a85a9bfd3b
Yasuharu NAKANO <ynak@jggug.org>
2013-09-12 20:34:04 +0900

    commented out "grails.controllers.defaultScope = 'singleton'"

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -41,7 +41,7 @@ grails.views.default.codec = "html"
 
 // The default scope for controllers. May be prototype, session or singleton.
 // If unspecified, controllers are prototype scoped.
-grails.controllers.defaultScope = 'singleton'
+//grails.controllers.defaultScope = 'singleton'
 
 // GSP settings
 grails {
