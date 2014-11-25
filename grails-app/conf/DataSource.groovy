1b23b71f96bb710046ac51497d3ae54f1d4493b2
Yasuharu Nakano <ynak@jggug.org>
2014-11-25 10:00:35 +0900

    Support H2 mode for trial

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,20 +1,76 @@
-dataSource {
-    driverClassName = "org.postgresql.Driver"
-    username = "postgres"
-    password = ""
-    dialect = irclog.helper.MyPostgreSQLDialect
-    pooled = true
-    properties {
-        maxActive = -1
-        minEvictableIdleTimeMillis = 1800000
-        timeBetweenEvictionRunsMillis = 1800000
-        numTestsPerEvictionRun = 3
-        testOnBorrow = true
-        testWhileIdle = true
-        testOnReturn = true
-        validationQuery = "SELECT 1"
-    }
+switch (System.getProperty("db")) {
+    case "h2":
+        println "Configuring for H2..."
+        dataSource {
+            driverClassName = "org.h2.Driver"
+            username = "sa"
+            password = ""
+            dbCreate = "create-drop"
+            pooled = true
+        }
+        environments {
+            development {
+                dataSource {
+                    dbCreate = "create"
+                    url = "jdbc:h2:mem:db/irclog_dev;MVCC=TRUE;LOCK_TIMEOUT=10000;MODE=PostgreSQL"
+                }
+            }
+            test {
+                dataSource {
+                    dbCreate = "create"
+                    url = "jdbc:h2:mem:irclog_test;MVCC=TRUE;LOCK_TIMEOUT=10000;MODE=PostgreSQL"
+                }
+            }
+            production {
+                dataSource {
+                    dbCreate = "validate"
+                    url = "jdbc:h2:file:db/irclog;MVCC=TRUE;LOCK_TIMEOUT=10000;MODE=PostgreSQL"
+                }
+            }
+        }
+        break
+
+    default:
+        println "Configuring for PostgreSQL..."
+        dataSource {
+            driverClassName = "org.postgresql.Driver"
+            username = "postgres"
+            password = ""
+            dialect = irclog.helper.MyPostgreSQLDialect
+            pooled = true
+            properties {
+                maxActive = -1
+                minEvictableIdleTimeMillis = 1800000
+                timeBetweenEvictionRunsMillis = 1800000
+                numTestsPerEvictionRun = 3
+                testOnBorrow = true
+                testWhileIdle = true
+                testOnReturn = true
+                validationQuery = "SELECT 1"
+            }
+        }
+        environments {
+            development {
+                dataSource {
+                    dbCreate = "create"
+                    url = "jdbc:postgresql://localhost:5432/irclog_dev"
+                }
+            }
+            test {
+                dataSource {
+                    dbCreate = "create"
+                    url = "jdbc:h2:mem:irclog_test;MVCC=TRUE;LOCK_TIMEOUT=10000;MODE=PostgreSQL"
+                }
+            }
+            production {
+                dataSource {
+                    dbCreate = "validate"
+                    url = "jdbc:postgresql://localhost:5432/irclog"
+                }
+            }
+        }
 }
+
 hibernate {
     cache.use_second_level_cache = false
     cache.use_query_cache = false
@@ -23,31 +79,4 @@ hibernate {
     use_sql_comments = true
     jdbc.batch_size = 50
 }
-// environment specific settings
-environments {
-    development {
-        //dataSource {
-        //    dbCreate = "create"
-        //    url = "jdbc:h2:file:db/irclog_dev;MVCC=TRUE;LOCK_TIMEOUT=10000;MODE=PostgreSQL"
-        //}
-        dataSource {
-            dbCreate = "create"
-            url = "jdbc:postgresql://localhost:5432/irclog_dev"
-        }
-    }
-    test {
-        dataSource {
-            driverClassName = "org.h2.Driver"
-            username = "sa"
-            password = ""
-            dbCreate = "create-drop"
-            url = "jdbc:h2:mem:irclog_test;MVCC=TRUE;LOCK_TIMEOUT=10000;MODE=PostgreSQL"
-            pooled = true
-        }
-    }
-    production {
-        dataSource {
-            url = "jdbc:postgresql://localhost:5432/irclog"
-        }
-    }
-}
+
