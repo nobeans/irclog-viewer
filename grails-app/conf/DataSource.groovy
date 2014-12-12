9ef1f80e55db2055169967aa1fa6bf6dc24d8b57
Yasuharu Nakano <ynak@jggug.org>
2014-12-12 10:33:41 +0900

    Fix database's pool properties becuase it caused vanishing database for h2:mem

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,28 +1,3 @@
-dataSource {
-    pooled = true
-    jmxExport = true
-    properties {
-        // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
-        jmxEnabled = true
-        initialSize = 5
-        maxActive = 50
-        minIdle = 5
-        maxIdle = 25
-        maxWait = 10000
-        maxAge = 10 * 60000
-        timeBetweenEvictionRunsMillis = 5000
-        minEvictableIdleTimeMillis = 60000
-        validationQuery = "SELECT 1"
-        validationQueryTimeout = 3
-        validationInterval = 15000
-        testOnBorrow = true
-        testWhileIdle = true
-        testOnReturn = false
-        jdbcInterceptors = "ConnectionState"
-        defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
-    }
-}
-
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
@@ -73,6 +48,28 @@ switch (System.getProperty("db")) {
             dialect = irclog.helper.MyPostgreSQLDialect
             username = "postgres"
             password = ""
+            pooled = true
+            jmxExport = true
+            properties {
+                // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
+                jmxEnabled = true
+                initialSize = 5
+                maxActive = 50
+                minIdle = 5
+                maxIdle = 25
+                maxWait = 10000
+                maxAge = 10 * 60000 // NOTE: The use of "maxAge" and "pool" for h2:mem causes vanishing database.
+                timeBetweenEvictionRunsMillis = 5000
+                minEvictableIdleTimeMillis = 60000
+                validationQuery = "SELECT 1"
+                validationQueryTimeout = 3
+                validationInterval = 15000
+                testOnBorrow = true
+                testWhileIdle = true
+                testOnReturn = false
+                jdbcInterceptors = "ConnectionState"
+                defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
+            }
         }
         environments {
             development {
