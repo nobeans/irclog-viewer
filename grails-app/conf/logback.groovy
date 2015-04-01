a0ef3c26d07fdb46213a447995488f223803cb67
Yasuharu Nakano <ynak@jggug.org>
2015-04-01 18:34:27 +0900

    Configure logback

--- grails-app/conf/logback.groovy
+++ grails-app/conf/logback.groovy
@@ -1,28 +1,25 @@
 import grails.util.BuildSettings
 import grails.util.Environment
 
-
 // See http://logback.qos.ch/manual/groovy.html for details on configuration
 appender('STDOUT', ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
-        pattern = "%level %logger - %msg%n"
+        pattern = /%d{yyyy-MM-dd HH:mm:ss.SSS} [%level] \(%thread\) \(%logger{1}:%line\) -- %msg%n/
     }
 }
 
-root(ERROR, ['STDOUT'])
+root info, ['STDOUT']
 
-if(Environment.current == Environment.DEVELOPMENT) {
+if (Environment.current == Environment.DEVELOPMENT) {
     def targetDir = BuildSettings.TARGET_DIR
-    if(targetDir) {
-
+    if (targetDir) {
         appender("FULL_STACKTRACE", FileAppender) {
-
             file = "${targetDir}/stacktrace.log"
             append = true
             encoder(PatternLayoutEncoder) {
                 pattern = "%level %logger - %msg%n"
             }
         }
-        logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false )
+        logger "StackTrace", ERROR, ['FULL_STACKTRACE'], false
     }
 }
